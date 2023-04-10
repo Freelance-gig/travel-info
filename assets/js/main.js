e84455992fd6156265ab85e99051e40344a5e804
tbello <bello4aus@gmail.com>
2023-04-10 07:35:06 +0100

    - add html structure for home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
+import countryJson from '../data.json' assert {type: 'json'};
 // https://rapidapi.com/richardarthur228/api/country-facts/ 
 // https://rapidapi.com/traveltables/api/cost-of-living-and-prices/ 
 const navBtn = document.querySelector('.mobile-nav-toggle')
 const nav = document.querySelector('.nav');
 const closeBtn = document.querySelector('.close-nav');
+const africaContainer = document.querySelector('.africa-container')
+const asiaContainer = document.querySelector('.asia-container')
+const europeContainer = document.querySelector('.europe-container')
+const oceaniaContainer = document.querySelector('.oceania-container')
+const americaContainer = document.querySelector('.america-container')
+const selectRegion = document.querySelector('#select-region')
+const countryFactsUrl = 'https://country-facts.p.rapidapi.com'
+const localStore = localStorage.getItem("countryFacts")
 
+console.log(selectRegion)
 navBtn.addEventListener('click', ()=>{
     nav.classList.add('nav-open');
 })
@@ -21,9 +31,7 @@ async function fetchData(url) {
     return jsonData
 }
 
-const localStore = localStorage.getItem("countryFacts")
 
-const countryFactsUrl = 'https://country-facts.p.rapidapi.com'
 
 // const africanCountries = fetchData(`${countryFactsUrl}/region/africa`)
 // console.log(africanCountries)
@@ -49,4 +57,33 @@ const countryFactsUrl = 'https://country-facts.p.rapidapi.com'
 //     }
 //     setLocalStorage()
 // }
-console.log('%RAPID_KEY%')
+
+const renderCountryFacts = (container, data) => {
+    data.slice(0, 4).forEach(element => {
+        container.innerHTML += `
+        <div class="card__container">
+            <img src=${element.flag} alt="team-logo" class='card__flag'/>
+            
+            <div class="card__data">
+                <h2> ${element.name.official} </h2>
+                <span> capital ${element.capital[0]} </span>
+                <span> population ${element.population} </span>
+            </div>
+        </div> 
+        `
+    });     
+}
+{/* <div>
+<h2> Africa </h2>
+<div  class="africa-container country__container">
+    
+</div>
+<button class="btn btn-secondary"> More </button>
+</div> */}
+renderCountryFacts(africaContainer, countryJson.africa)
+// renderCountryFacts(asiaContainer, countryJson.asia)
+// renderCountryFacts(europeContainer, countryJson.europe)
+// renderCountryFacts(oceaniaContainer, countryJson.oceania)
+// renderCountryFacts(americaContainer, countryJson.america)
+
+
